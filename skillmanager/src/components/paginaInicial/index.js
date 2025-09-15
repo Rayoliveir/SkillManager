9fbfc50dc9d47cdef16adc9e72612b2d587f77d7
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2025-09-15 18:28:56 +0000

    Criando projeto react

---
+++ skillmanager/src/components/paginaInicial/index.js
@@ -0,0 +1,10 @@
+import './styles.css'
+
+function PaginaInicial() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default PaginaInicial
---
+++ skillmanager/src/pages/dashboard/index.js
@@ -0,0 +1,13 @@
+import './styles.css'
+import PaginaInicial from
+
+
+function Dashboard() {
+    return (
+        <div className="pag_inicial-container">
+            <Dashboard />
+        </div>
+    );
+}
+
+export default Dashboard
