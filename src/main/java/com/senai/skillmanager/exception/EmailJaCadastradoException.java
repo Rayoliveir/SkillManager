49dc8a70c564233fe839bd66ef042e10f4be57a4
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-10-12 23:41:36 -0300

    Delete src directory

--- src/main/java/com/senai/skillmanager/exception/EmailJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.senai.skillmanager.exception;
-
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String message) {
-        super(message);
-    }
-}
