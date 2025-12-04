4147c0cf5b256a7e85b087fc8c05e5ed87dfd766
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-12-04 01:05:06 -0300

    Delete src directory

--- src/main/java/com/senai/skillmanager/repository/DadosAcademicosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.skillmanager.repository;
-
-import com.senai.skillmanager.model.estagiario.DadosAcademicos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DadosAcademicosRepository extends JpaRepository<DadosAcademicos, Long> {
-}
